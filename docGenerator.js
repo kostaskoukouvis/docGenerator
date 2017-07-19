f491a95d3b5083d18422288b598789b042b7168d
Konstantinos Koukouvis <konstantinos@appspotr.com>
2017-07-20 00:40:34 +0300

    basic functionality & format

--- docGenerator.js
+++ docGenerator.js
@@ -1,6 +1,15 @@
 (function() {
 	var docGenerator;
 
+	(function(root) {
+		var ref;
+		if ((ref = root.bundle) != null ? ref.minApiVersion('0.2.0') : void 0) {
+		  return root.Mustache = require("./mustache");
+		} else {
+		  return require("mustache.js");
+		}
+	})(this);
+
 	docGenerator = function() {
 		this.request = function(req) {
 			var headers = [];
@@ -9,22 +18,49 @@
 			var k, v;
 			for (k in req.headers) {
 				v = req.headers[k];
+				if (k === "Content-Type") {
+					is_json = v.search(/(json)/i) > -1;
+					continue;
+				}
 				headers.push({ key: k, value: v});
 			}
+
 			if (req.body.length > 0) {
-				for (k in req.body) {
-					v = req.body[k];
+				var requestBody = req.body
+				if (is_json) {
+					requestBody = JSON.parse(requestBody);
+				}
+				for (k in requestBody) {
+					v = requestBody[k];
 					body.push({ key: k, value: v});
 				};
 			}
 			return {
+				"headers?": headers.length > 0,
 				headers: headers,
+				"body?": body.length > 0,
 				body: body
 			};
 		};
 
 		this.response = function(res) {
-			return res;
+			if (res.responseBody.length > 0) {
+				var k, v, val;
+				var body = [];
+				var resBody = JSON.stringify(JSON.parse(res.responseBody), null, 4);
+				// if (is_json) {
+				// 	requestBody = JSON.parse(requestBody);
+				// }
+				// for (k in resBody) {
+				// 	v = resBody[k];
+				// 	body.push({ key: k, value: v});
+				// };
+			}
+			return {
+				statusCode: res.responseStatusCode,
+				"body?": true,
+				body: resBody
+			};
 		};
 
 		this.path = function(url) {
